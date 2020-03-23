1c17e2e99883a4eec1f013d098157f60bd8dedc5
whitneyxlachelle <whiggins16@yahoo.com>
2020-03-22 19:27:29 -0500

    axioswithauth

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,36 +1,48 @@
 import React, { useState } from "react";
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
 const Login = (props) => {
+  console.log(props);
   const [credentials, setCredentials] = useState({});
- 
 
-   const handleChange = event => {
-     event.preventDefault();
-       setCredentials({
-         ...credentials,
-         [event.target.name]: event.target.value,
-       })
-   }
+
+  //save token to localStorage
+  const handleSubmit = event => {
+    event.preventDefault();
+    axiosWithAuth()
+      .post('http://localhost:5000/api/login', credentials)
+      .then(response => {
+        localStorage.setItem('token', response.data.payload);
+      })
+  }
+
+  const handleChange = event => {
+    event.preventDefault();
+    setCredentials({
+      ...credentials,
+      [event.target.name]: event.target.value,
+    })
+  }
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-     <div>
-       <form>
-         <input 
-         type="text"
-         name="username"
-         placeholder="Username"
-         onChange={handleChange}
-         />
-           <input 
-         type="password"
-         name="password"
-         placeholder="Password"
-         onChange={handleChange}
-         />
-         <button>Log in</button>
-       </form>
-     </div>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="username"
+            placeholder="Username"
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            onChange={handleChange}
+          />
+          <button>Log in</button>
+        </form>
+      </div>
     </>
   );
 };
